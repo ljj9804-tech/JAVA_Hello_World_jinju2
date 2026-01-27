3d073bf2624ac2ccee0933549ba2d977b21499f8
leejinju <ljj9804@gamil.com>
2026-01-27 21:28:27 +0900

    20260127_업데이트1

---
+++ src/A01_MyNote/A04_Note.java
@@ -0,0 +1,31 @@
+package A01_MyNote;
+
+public class A04_Note {
+    public static void main(String[] args) {
+        //산술 연산자
+        int num1 = 69/10;
+        System.out.println(num1); //몫 = 6
+
+        int num2 = 69%10;
+        System.out.println(num2); //나머지 = 9
+
+
+
+
+        //대입 연산자
+
+
+        //증감 연산자
+
+
+        //비교 연산자
+
+
+        //논리 연산자
+
+
+        //삼항 연산자
+
+
+    }
+}
