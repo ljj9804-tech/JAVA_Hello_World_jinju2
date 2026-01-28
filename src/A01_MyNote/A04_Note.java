27c7052d7b2bd8c6d6375f48d3a4f41a4bdaa5f7
leejinju <ljj9804@gmail.com>
2026-01-28 17:49:21 +0900

    20260128_업데이트1

--- src/A01_MyNote/A04_Note.java
+++ src/A01_MyNote/A04_Note.java
@@ -3,19 +3,52 @@ package A01_MyNote;
 public class A04_Note {
     public static void main(String[] args) {
         //산술 연산자
-        int num1 = 69/10;
-        System.out.println(num1); //몫 = 6
+//        int n = 4;
+//        System.out.println(n % 2);
+        //나머지가 0이면 n은 짝수
+        //나머지가 1이면 n은 홀수
 
-        int num2 = 69%10;
-        System.out.println(num2); //나머지 = 9
+//
+//        System.out.println(69/10); //몫 = 6
+//        System.out.println(69%10); //나머지 = 9
 
+//        System.out.println(1+1);
+//        System.out.println(2-1);
+//        System.out.println(2*2);
 
 
 
         //대입 연산자
+//        int a = 10;
+//
+//        // 1. 더하기 대입 (+=)
+//        a += 5; // [a = a + 5]와 동일
+//        System.out.println("10 + 5 = " + a); // 출력: 15
+//
+//        // 2. 빼기 대입 (-=)
+//        a -= 3; // [a = a - 3]과 동일
+//        System.out.println("15 - 3 = " + a); // 출력: 12
+//
+
+//        int a =1, b = 3;
+//        a = b; //a 변수 안에 b의 값(3)을 넣음
+//        System.out.println(a);
+
 
 
         //증감 연산자
+//        int a = 1;
+//        int b = ++a; //b=2, a=2
+//
+//        System.out.println(b);
+//        System.out.println(a);
+
+        //증감 연산자
+//        int a = 1;
+//        int b = a++; //b=1, a=2
+//
+//        System.out.println(b);
+//        System.out.println(a);
 
 
         //비교 연산자
@@ -25,6 +58,12 @@ public class A04_Note {
 
 
         //삼항 연산자
+        int a = 5, b = 3;
+        int big = (a > b)? a : b;
+        /*조건 연산자 값이 참(true)이므로
+        변수에 a 값을 넣음*/
+
+        System.out.println(big);
 
 
     }
